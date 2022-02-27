9f18ced71aa7cce5a44fe6f1db49b25005a47702
24SVEN <johnahn0124@gmail.com>
2022-02-26 22:57:18 -0500

    solved problem 3 but timeout error. Need to include dp

---
+++ problem2.py
@@ -0,0 +1,56 @@
+from cgitb import small
+
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        smaller_string,larger_string = '',''
+
+        
+
+        if len(s)> len(t):
+            smaller_string = s
+            larger_string = t
+        else:
+            larger_string = s
+            smaller_string = t
+
+        count = 0
+        large_string_count = len(larger_string)
+        #find missing characters from smaller and larger
+        #guess find larger first
+
+        ld = {}
+        sd = {}
+
+        #convert strings to dictionaries
+        for char in larger_string:
+            if char in ld:
+                ld[char] += 1
+            else:
+                ld[char] = 1
+        
+
+        for char in smaller_string:
+            if char in sd:
+                sd[char] += 1
+            else:
+                sd[char] = 1
+
+        for char in smaller_string:
+            if char in ld:
+                ld[char] -= 1
+                if ld[char] == 0:
+                    del ld[char]
+            else:
+                count += 1
+
+        
+
+        diff = large_string_count + count - len(smaller_string)
+        count = diff+ count
+        print(count)
+        return count
+
+test = Solution()
+test.minSteps('leetcode','coats')
+test.minSteps('night','thing')
---
+++ problem3.py
@@ -0,0 +1,33 @@
+import math
+class Solution:
+    def minimumTime(self, time, totalTrips: int) -> int:
+        dp = totalTrips * [0]
+
+        trip_count = 0
+
+        return self.dfs(time,totalTrips,1,0)
+
+        
+    def dfs(self,time,totalTrips,counter_time,current_trips):
+
+
+        current_trips = 0
+        for bus in time[0:counter_time]:
+            current_trips += math.trunc(counter_time/bus)
+        
+        
+        if totalTrips <= current_trips:
+            return counter_time
+        else:
+            counter_time += 1
+            return self.dfs(time,totalTrips, counter_time,current_trips)
+
+
+
+test = Solution()
+# print(test.minimumTime([1,2,3],5))
+#3
+# print(test.minimumTime([2],1))
+# #2
+
+print(test.minimumTime([9,3,10,5],2))
